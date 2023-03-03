fae812f30b902dba6420dc9d52afe403f97a047f
John F Morton <morton@jmx2.com>
2023-03-03 13:29:58 -0500

    corrected base URL

--- vite.demo.config.js
+++ vite.demo.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: 'https://johnfmorton.github.io/vite-module-builder-w-ghpages-npm-template/', /// Set this to './' if your site is deployed at https://<USERNAME>.github.io/<REPO>/.
+    base: 'https://johnfmorton.github.io/progressive-share-button/', /// Set this to './' if your site is deployed at https://<USERNAME>.github.io/<REPO>/.
     build: {
         outDir: 'demo',
         minify: true,
