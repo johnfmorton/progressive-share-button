525bc97983e13b54c234293fcb41bced082e25d3
John F Morton <morton@jmx2.com>
2023-03-03 11:23:22 -0500

    Use module name, progressive-share-button, throughout

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ const path = require('path')
 const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'lib/main.ts'),
-      name: 'progressive-share-button',
-      fileName: (format) => `progressive-share-button.${format}.js`
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'lib/progressive-share-button.ts'),
+            name: 'progressive-share-button',
+            fileName: (format) => `progressive-share-button.${format}.js`,
+        },
+        minify: false,
     },
-    minify: false
-  }
-});
+})
